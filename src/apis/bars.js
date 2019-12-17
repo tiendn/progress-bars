e2a1340b08755b348418779bf620d21a076b5ae9
Tiendn <daonamtien1@gmail.com>
2019-12-17 08:17:56 +0700

    Use backend https

--- src/apis/bars.js
+++ src/apis/bars.js
@@ -3,5 +3,5 @@ import Request from './request';
 const END_POINT = process.env.REACT_APP_END_POINT;
 
 export const getBars = () => {
-  return Request.get(`${END_POINT}/bars/`);
+  return Request.get(`${END_POINT}/bars`);
 };
