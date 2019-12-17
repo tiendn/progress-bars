a259d4d51e01403aee3809c1f68aa07c0bc441c6
Tiendn <daonamtien1@gmail.com>
2019-12-17 21:21:48 +0700

    fix critical bug

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 const render = () => {
-  ReactDOM.render(App, document.getElementById('root'));
+  ReactDOM.render(<App />, document.getElementById('root'));
 };
 
 if (process.env.NODE_ENV === 'development' && module.hot) {
@@ -18,4 +18,4 @@ render();
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister();git ad
